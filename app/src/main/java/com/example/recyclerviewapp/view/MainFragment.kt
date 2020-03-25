4fbbfeb2da0fee09f8c0621c13d81380fa1ea9f8
Benedict <benedictcontawe@gmail.com>
2020-03-25 10:47:54 +0800

    Updating ViewModelProvider, and gradle

--- app/src/main/java/com/example/recyclerviewapp/view/MainFragment.kt
+++ app/src/main/java/com/example/recyclerviewapp/view/MainFragment.kt
@@ -6,8 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+//import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewapp.*
@@ -33,7 +34,7 @@ class MainFragment : Fragment(), CustomListeners {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        viewModel = ViewModelProviders.of(getActivity()!!, CustomViewModelFactory(context!!, this)).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(getActivity()!!, CustomViewModelFactory(context!!, this)).get(MainViewModel::class.java)
         binding.viewModel = viewModel
 
         viewModel.setItems()
