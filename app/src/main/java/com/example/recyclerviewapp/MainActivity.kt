845da44200b280cb249bda3a50cee939b746e0c3
Benedict <benedictcontawe@gmail.com>
2020-06-18 01:10:50 +0800

    Add Recycler View Detector for Scroll Top and Bottom

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.recyclerviewapp
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity(), CustomListeners {
     private lateinit var itemList : MutableList<CustomViewModel>
 
     companion object {
+        private var TAG : String = MainActivity::class.java.simpleName
+
         fun newIntent(context : Context) : Intent {
             val intent : Intent = Intent(context, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -34,6 +38,7 @@ class MainActivity : AppCompatActivity(), CustomListeners {
         recycler_view.setLayoutManager(CustomLinearLayoutManager(this, LinearLayout.VERTICAL, false))
         recycler_view.setAdapter(adapter)
         recycler_view.setHasFixedSize(true)
+        setScrollListener()
 
         adapter.setActivity(this)
     }
@@ -48,6 +53,31 @@ class MainActivity : AppCompatActivity(), CustomListeners {
         itemList.add(CustomViewModel(R.drawable.ic_person_white, "E"))
         itemList.add(CustomViewModel(R.drawable.ic_person_white, "F"))
         itemList.add(CustomViewModel(R.drawable.ic_person_white, "G"))
+        itemList.add(CustomViewModel(R.drawable.ic_person_white, "H"))
+        itemList.add(CustomViewModel(R.drawable.ic_person_white, "I"))
+        itemList.add(CustomViewModel(R.drawable.ic_person_white, "J"))
+        itemList.add(CustomViewModel(R.drawable.ic_person_white, "K"))
+        itemList.add(CustomViewModel(R.drawable.ic_person_white, "L"))
+        itemList.add(CustomViewModel(R.drawable.ic_person_white, "M"))
+        itemList.add(CustomViewModel(R.drawable.ic_person_white, "N"))
+        itemList.add(CustomViewModel(R.drawable.ic_person_white, "O"))
+        itemList.add(CustomViewModel(R.drawable.ic_person_white, "P"))
+    }
+
+    private fun setScrollListener() {
+        recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                when {
+                    recyclerView.canScrollVertically(-1) == false -> {
+                        Log.d(TAG,"Recycler View top reached")
+                    }
+                    recyclerView.canScrollVertically(1) == false -> {
+                        Log.d(TAG,"Recycler View bottom reached")
+                    }
+                }
+            }
+        })
     }
 
     override fun onStart() {
