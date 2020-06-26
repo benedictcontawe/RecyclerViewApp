520a382119d514c446fb2e012599d866bf225a90
Benedict <benedictcontawe@gmail.com>
2020-06-26 13:38:45 +0800

    Update this branch, add RecyclerView OnScrollListener

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity(), CustomListeners {
 
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity(), CustomListeners {
     private lateinit var itemList : MutableList<CustomViewModel>
 
     companion object {
+        private var TAG : String = MainActivity::class.java.simpleName
+
         fun newIntent(context : Context) : Intent {
             val intent : Intent = Intent(context, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -54,11 +57,51 @@ class MainActivity : AppCompatActivity(), CustomListeners {
         adapter.setItems(itemList)
     }
 
+    override fun onResume() {
+        super.onResume()
+        recycler_view.addOnScrollListener(setScrollListener())
+    }
+
+    override fun onPause() {
+        super.onPause()
+        recycler_view.removeOnScrollListener(setScrollListener())
+    }
+
     override fun onClick(item: CustomViewModel, position: Int) {
         Toast.makeText(this,"onClick " + item.name + " " + position,Toast.LENGTH_SHORT).show()
     }
 
     override fun onLongClick(item: CustomViewModel, position: Int) {
         Toast.makeText(this,"onLongClick " + item.name + " " + position,Toast.LENGTH_SHORT).show();
+    }
+
+    private fun setScrollListener() : RecyclerView.OnScrollListener {
+        return object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val canScrollUp : Boolean = recyclerView.canScrollVertically(-1)
+                val canScrollDown : Boolean = recyclerView.canScrollVertically(1)
+                when {
+                    canScrollUp && canScrollDown -> {
+                        Log.d(TAG,"Recycler View at Middle")
+                    }
+                    canScrollDown && !canScrollUp -> {
+                        Log.d(TAG,"Recycler View top reached")
+                    }
+                    canScrollUp && !canScrollDown -> {
+                        Log.d(TAG,"Recycler View bottom reached")
+                    }
+                }
+
+                when {
+                    dy > 0 -> {
+                        Log.d(TAG,"Recycler View Scrolling Down")
+                    }
+                    dy < 0 -> {
+                        Log.d(TAG,"Recycler View Scrolling Up")
+                    }
+                }
+            }
+        }
     }
 }
