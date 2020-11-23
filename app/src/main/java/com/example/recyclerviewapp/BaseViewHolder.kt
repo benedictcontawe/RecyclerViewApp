ba0cfcb54536f4e2dcd28fcb21e070b5a828d635
Benedict <benedictcontawe@gmail.com>
2020-11-23 22:20:27 +0800

    Cleaning Code for this branch.

--- app/src/main/java/com/example/recyclerviewapp/BaseViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewapp/BaseViewHolder.kt
@@ -3,7 +3,6 @@ package com.example.recyclerviewapp
 import android.content.Context
 import android.graphics.Point
 import android.util.Log
-import android.util.TypedValue
 import android.view.Display
 import android.view.View
 import android.view.WindowManager
