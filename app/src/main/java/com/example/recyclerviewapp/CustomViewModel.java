0d92736313e84a913030b9faf548356709a6bc91
Benedict Contawe <benedictcontawe@gmail.com>
2024-04-17 19:44:59 +0800

    added some xml

--- app/src/main/java/com/example/recyclerviewapp/CustomViewModel.java
+++ app/src/main/java/com/example/recyclerviewapp/CustomViewModel.java
@@ -3,7 +3,6 @@ package com.example.recyclerviewapp;
 class CustomViewModel {
     int icons;
     String names;
-
     public CustomViewModel(int icons, String names) {
         this.icons = icons;
         this.names = names;
